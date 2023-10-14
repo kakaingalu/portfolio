e6906be4721ec2334b2479f335292d8c50e9a2cc
Kakai Ngalu <josephngalu96@gmail.com>
2023-10-14 23:14:54 +0300

    create NavBar, body and footer

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import NavBar from './Components/NavBar.js';
+import Body from './Components/Body.js';
+import Footer from './Components/Footer';
+
+function App() {
+  return (
+    <div className="App">
+     <NavBar />
+     <hr />
+     <Body />
+     <hr />
+     <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Body.js
@@ -0,0 +1,46 @@
+import logo from '../Assets/11zon_cropped.png';
+
+function Body() {
+    return (
+        <div className="body">
+            <h1>Welcome To My Frontend Universe</h1>
+
+            <img src={logo} className="App-logo" alt="logo" />
+
+            <p>I am <strong>Kakai Ngalu</strong> a 
+            <strong> Frontend </strong> enthusiast with 
+            passion to create seamless digital experience.</p>  
+
+            <h1>Why i’m here</h1>
+
+            <p>This is not just a website but a canvas playground 
+            and also my portfolio. I have a mix of skills in code 
+            and creativity to showcase what i do best in creating 
+            interactive and visual appealing websites. </p>
+
+            <h1>What drives me</h1>
+
+            <p>i believe in the power of tech to transform ideas 
+            into reality. Through frontend development, i breathe 
+            life into designs making them intuitive and engaging. 
+            My purpose? To create delightful user experience that 
+            leaves a mark. </p>
+
+            <h1>Let’s collaborate!</h1>
+
+            <p>whether you are a fellow coder, a protentional 
+            client or just someone curious about the world of 
+            frontend, you’re in the right place. Dive into my 
+            projects, explore the art of coding and let’s embark on 
+            this tech adventure together. </p>
+
+            <p>Ready to redefine digital boundaries? Let’s get 
+            started.</p>
+
+            <h4>Happy Coding</h4>
+            <h5>Kakai Ngalu🚀</h5>
+        </div>
+    )
+}
+
+export default Body;
