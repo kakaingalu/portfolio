a0c62a5d27eec94330673487890b9d10ddd8e0da
Kakai Ngalu <josephngalu96@gmail.com>
2023-10-19 09:27:25 +0300

    update menu

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,22 +1,63 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { useMediaQuery } from '@mui/material';
 import logo from '../../Assets/Kaks_Logo.png';
 
 function NavBar() {
+  const isSmallScreen = useMediaQuery('(max-width:768px)');
+  const [showMenu, setShowMenu] = useState(false);
+
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
+
   return (
     <div className="NavBar">
-      <Link to="/" >
-       <img src={logo} className="App-logo-Nav" alt="logo" />
+      <Link to="/">
+        <img src={logo} className="App-logo-Nav" alt="logo" />
       </Link>
-      <h1>
-        <Link to="/" className='link' >About</Link>
-      </h1>
-      <h1>
-      <Link to="/projects" className='link'>Projects</Link>
-      </h1>
-      <h1>
-      <Link to="/contacts" className='link' >Contacts</Link>
-      </h1>
+      {isSmallScreen ? (
+        <>
+          <button onClick={toggleMenu}>Menu</button>
+          {showMenu && (
+            <ul className="NavMenu">
+              <li>
+                <Link to="/" className="link">
+                  About
+                </Link>
+              </li>
+              <li>
+                <Link to="/projects" className="link">
+                  Projects
+                </Link>
+              </li>
+              <li>
+                <Link to="/contacts" className="link">
+                  Contacts
+                </Link>
+              </li>
+            </ul>
+          )}
+        </>
+      ) : (
+        <ul className="NavBar">
+          <h1>
+            <Link to="/" className="link NavBar">
+              About
+            </Link>
+          </h1>
+          <h1>
+            <Link to="/projects" className="link NavBar">
+              Projects
+            </Link>
+          </h1>
+          <h1>
+            <Link to="/contacts" className="link NavBar">
+              Contacts
+            </Link>
+          </h1>
+        </ul>
+      )}
     </div>
   );
 }
